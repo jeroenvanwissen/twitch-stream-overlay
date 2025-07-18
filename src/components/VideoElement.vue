aea3d1591871b7f9c70b2803307f4ebccc07cf33
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-07-18 15:00:08 +0200

    fix(VideoElement): correct casing for hasVideo variable in import and usage

--- src/components/VideoElement.vue
+++ src/components/VideoElement.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { hasvideo, video } from '@/store/blerps';
+import { hasVideo, video } from '@/store/blerps';
 </script>
 
 <template>
 	<div class="absolute w-1/3 left-1/2 -translate-x-1/2 h-auto aspect-video"
 		:class="{
-			'opacity-0': !hasvideo,
+			'opacity-0': !hasVideo,
 		}"
 	>
 		<video ref="video" src="" class="w-full h-full rounded-lg shadow-lg" />
