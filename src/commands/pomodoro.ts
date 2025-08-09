19d2aa0c6fae05149c9bf13bfc5d59ca9c9e9050
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2025-08-09 18:28:41 +0200

    Pomodoro command update

--- src/commands/pomodoro.ts
+++ src/commands/pomodoro.ts
@@ -7,11 +7,11 @@ const command: Command = {
   type: 'command',
   storage: {},
   init: () => {},
-  callback: async ({ channel }) => {
-    await chatClient.say(
-      channel,
-      `I try to work with the Pomodoro technique to stay focused. Most of the time I'll be focused at work for 50 minutes before taking a 10 minute break. Lunch breaks will be around 1hour. But, most of the times I just forget the breaks and when I do remember them, I just take a too long break...`
-    )
+  callback: async ({ channel, message }) => {
+    const text = `I try to work with the Pomodoro technique to stay focused. Most of the time I'll be focused at work for 50 minutes before taking a 10 minute break. Lunch breaks will be around 1hour. But, most of the times I just forget the breaks and when I do remember them, I just take a too long break...`
+    await chatClient.say(channel, text, {
+      replyTo: message.id
+    })
   }
 }
 
